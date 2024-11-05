e6b06a45aa1f56d587570784dfb3992a74dfc5eb
d33fur <d33fur@gmail.com>
2024-11-05 19:04:46 +0300

    add: 33routes

---
+++ 33routes/install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y \
+    git \
+    make
+
+# 
+# nginx
+# 
+sudo apt update
+sudo apt install nginx -y
+
+rm /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf
+cp 33routes.ru.crt /etc/ssl/33routes.ru.crt
+cp 33routes.ru.key /etc/ssl/33routes.ru.key
+cp ca.crt /etc/ssl/ca.crt
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# 
+# docker
+# 
+sudo apt update
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+    -o /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) \
+    signed-by=/etc/apt/keyrings/docker.asc] \
+    https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# 
+# portainer
+# 
+docker volume create portainer_data
+docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name=portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.21.4-alpine
+
+# 
+# jenkins
+# 
+docker run -d \
+    -p 9090:8080 \
+    -p 50000:50000 \
+    --name=jenkins \
+    --restart=always \
+    -v jenkins_home:/var/jenkins_home \
+    -e JENKINS_OPTS="--prefix=/admin/jenkins" \
+    jenkins/jenkins:alpine3.20-jdk21
+# sudo usermod -aG docker jenkins
+
+# vless x-ui
+git clone https://github.com/MHSanaei/3x-ui.git && \
+cd 3x-ui && \
+docker run -itd \
+    -e XRAY_VMESS_AEAD_FORCED=false \
+    -v $PWD/db/:/etc/x-ui/ \
+    -v $PWD/cert/:/root/cert/ \
+    --network=host \
+    --restart=unless-stopped \
+    --name=3x-ui \
+    ghcr.io/mhsanaei/3x-ui:latest
